20e778246b03c532a594a387c4846ac5e7b17c2c
Mateus_Munis <mateusfreitas2211@gmail.com>
2024-05-29 09:50:14 -0300

    push inicial do desafio Simulando Uma Conta Bancária Através Do Terminal/Console

---
+++ ContaBancaria/src/ContaTerminal.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int numeroConta;
+        float saldo;
+        String agencia, nomeCliente;
+
+        System.out.print("Digite o numero da conta: ");
+        numeroConta = input.nextInt();
+        System.out.print("Digite o numero da agencia com hifen(-): ");
+        agencia = input.next();
+        System.out.print("Digiet seu nome: ");
+        nomeCliente = input.next();
+        System.out.print("Insira o valor de seu saldo atual: ");
+        saldo = input.nextFloat();
+
+        System.out.printf("Ola %s, obrigado por criar uma conta em nosso banco, sua agencia e %s, conta %d e seu saldo %.2f ja esta disponivel para saque", nomeCliente, agencia, numeroConta, saldo);
+
+
+    }
+}
